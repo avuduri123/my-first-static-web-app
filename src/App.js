bf6a871e344e939a9871f3d56326e3f72c3d4447
avuduri123 <118868618+avuduri123@users.noreply.github.com>
2023-09-11 22:45:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Lab1Image from 'Lab1.png';
 
 function App() {
-  const value = 'World';
   return <div>
       <p>I did my homework!</p>
       <img src={Lab1Image} alt="lab1 image"/>
