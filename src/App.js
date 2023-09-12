44ef6f9360b3a658c7c584e5e0b9a0bd92bbbdc3
avuduri123 <118868618+avuduri123@users.noreply.github.com>
2023-09-11 23:01:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import Lab1Image from 'Lab1.png';
+import './App.css';
+import Lab1Image from './Lab1.png'; 
 
 function App() {
-  return <div>
-      <p>I did my homework!</p>
-      
-    </div>;
+  return (
+    <div>
+      <p>
+        I did my homework!
+      </p>
+      <img src={Lab1Image} width="1000" height="500" alt="Lab1"/>
+
+    </div>
+  );
 }
 
 export default App;
